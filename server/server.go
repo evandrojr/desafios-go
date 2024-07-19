4a105ccd2e127b347afc89ff568d314423d247e9
Evandro Jr <evandrojr@gmail.com>
2024-07-19 12:12:27 -0300

    Gravando no banco

--- server/server.go
+++ server/server.go
@@ -44,7 +44,7 @@ func initializeDB() (*sql.DB, error) {
 	query := `
     CREATE TABLE IF NOT EXISTS cotacoes (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name cotacao
+        cotacao REAL
     );`
 	_, err = db.Exec(query)
 	if err != nil {
@@ -59,19 +59,19 @@ func logConsoleAndBrowser(msg string, w http.ResponseWriter) {
 	w.Write([]byte(msg))
 }
 
-func getCotacao(ctxBg context.Context, data Cotacao) (Cotacao, error) {
-	ctx, cancel := context.WithTimeout(ctxBg, 200*time.Millisecond)
+func getCotacao(ctxBg context.Context, data *Cotacao) (*Cotacao, error) {
+	ctx, cancel := context.WithTimeout(ctxBg, 500*time.Millisecond)
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
-		errPersonalizado := errors.New("Erro ao preparar a requisição:" + err.Error())
-		return Cotacao{}, errPersonalizado
+		errPersonalizado := errors.New("Erro ao preparar a requisição: " + err.Error())
+		return &Cotacao{}, errPersonalizado
 	}
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		errPersonalizado := errors.New("Erro ao fazer a requisição:" + err.Error())
-		return Cotacao{}, errPersonalizado
+		errPersonalizado := errors.New("Erro ao fazer a requisição: " + err.Error())
+		return &Cotacao{}, errPersonalizado
 	}
 
 	defer res.Body.Close()
@@ -79,17 +79,17 @@ func getCotacao(ctxBg context.Context, data Cotacao) (Cotacao, error) {
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		errPersonalizado := errors.New("Erro ao ler a requisição:" + err.Error())
-		return Cotacao{}, errPersonalizado
+		return &Cotacao{}, errPersonalizado
 	}
 
 	err = json.Unmarshal(
 		body,
-		&data,
+		data,
 	)
 
 	if err != nil {
-		errPersonalizado := errors.New("Erro json.Unmarshal:" + err.Error() + err.Error())
-		return Cotacao{}, errPersonalizado
+		errPersonalizado := errors.New("Erro json.Unmarshal: " + err.Error() + err.Error())
+		return &Cotacao{}, errPersonalizado
 	}
 
 	// // jsonString:= fmt.Sprintf(res)
@@ -97,15 +97,15 @@ func getCotacao(ctxBg context.Context, data Cotacao) (Cotacao, error) {
 	return data, nil
 }
 
-func saveCotacao(ctx context.Context, data Cotacao) error {
+func saveCotacao(ctx context.Context, data *Cotacao) error {
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Millisecond)
 	defer cancel()
 
-	err := insertWithTimeout(ctx, db, data.Usdbrl.Bid)
+	err := insertWithTimeout(ctx, db, (*data).Usdbrl.Bid)
 	if err != nil {
-		fmt.Println("Failed to insert:", err)
+		fmt.Println("Erro ao inserir:", err)
 	} else {
-		fmt.Println("Insert successful")
+		fmt.Println("Inserido com sucesso")
 	}
 	return nil
 }
@@ -122,9 +122,9 @@ func cotacaoHandler(w http.ResponseWriter, r *http.Request) {
 	var data Cotacao
 	ctxRequisicao := context.Background()
 
-	data, err := getCotacao(ctxRequisicao, data)
+	_, err := getCotacao(ctxRequisicao, &data)
 	if err != nil {
-		logConsoleAndBrowser("Erro getCotacao:"+err.Error(), w)
+		logConsoleAndBrowser("Erro getCotacao: "+err.Error(), w)
 		return
 	} else {
 		pretty.Println(data)
@@ -136,9 +136,9 @@ func cotacaoHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Criei um novo contexto, pois se usasse o outro acho que poderia dar problema com 2 timeouts
 	ctxSalvamento := context.Background()
-	err = saveCotacao(ctxSalvamento, data)
+	err = saveCotacao(ctxSalvamento, &data)
 	if err != nil {
-		logConsoleAndBrowser("Erro SaveCotacao:"+err.Error(), w)
+		logConsoleAndBrowser("Erro SaveCotacao: "+err.Error(), w)
 	}
 	pretty.Println(data)
 
@@ -156,6 +156,10 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /cotacao/", cotacaoHandler)
 	pretty.Println("Rodando na porta:", port)
-	http.ListenAndServe("localhost:"+port, mux)
+
+	err = http.ListenAndServe("localhost:"+port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 	pretty.Println("Saindo")
 }
