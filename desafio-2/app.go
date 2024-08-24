ccac2dc63f1cdb7ddc11193f47d3c897dbce43fd
Evandro Jr <evandrojr@gmail.com>
2024-08-24 08:12:51 -0300

    inicio d2

--- desafio-2/app.go
+++ desafio-2/app.go
@@ -1,13 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+const cep = "41830430"
+
 func main() {
 
+	requestURL := fmt.Sprintf("https://brasilapi.com.br/api/cep/v1/%s", cep)
+	res, err := http.Get(requestURL)
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("client: got response!\n")
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+
 }
 
-func cep1() {
+// func cep1() {
 
-}
+// }
 
-func cep2() {
+// func cep2() {
 
-}
+// }
---
+++ desafio-2/go.mod
@@ -0,0 +1,3 @@
+module desafio-2
+
+go 1.22.5
