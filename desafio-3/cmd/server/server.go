fb8bc9608f30faad77e003544d2bce55d0abfe35
Evandro Junior <evandrojr@gmail.com>
2024-10-05 09:49:12 -0300

    erro servidor

--- desafio-3/cmd/server/server.go
+++ desafio-3/cmd/server/server.go
@@ -1,27 +1,44 @@
 package main
 
 import (
-	"desafio-3/graph"
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
+	"desafio-3/internal/database"
+
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	// courseDb := database.NewCourse(db)
+
+	fmt.Println(categoryDb)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	// srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{
+	// 	CategoryDB: categoryDb,
+	// 	CourseDB:   courseDb,
+	// }}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	// http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
