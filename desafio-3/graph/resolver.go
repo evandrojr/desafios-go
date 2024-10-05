e516610304526a617099c0becb54541819633938
Evandro Junior <evandrojr@gmail.com>
2024-10-05 09:10:11 -0300

    salvando

--- desafio-3/graph/resolver.go
+++ desafio-3/graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "desafio-3/internal/database"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	CategoryDb *database.Category
+}
