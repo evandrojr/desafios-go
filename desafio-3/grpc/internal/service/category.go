a1bfd9b6208f44470c7b9dc86d74381c9fd65bb6
Evandro Jr <evandrojr@gmail.com>
2024-10-14 18:39:42 -0300

    grpc

---
+++ desafio-3/grpc/internal/service/category.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"context"
+	"grpc/internal/database"
+	"grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return category, nil
+}
